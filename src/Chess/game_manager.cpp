a628157ad1e593c973ff9f2cf57ec8c569319965
iradeis <taianc2@illinois.edu>
2026-04-16 18:13:13 -0500

    game manager except for updateBoard()

--- src/Chess/game_manager.cpp
+++ src/Chess/game_manager.cpp
@@ -2,13 +2,30 @@
 
 GameManager game;
 
-GameManager::GameManager() : currentState(SystemState::MAIN_MENU) {
-    
-}
+GameManager::GameManager() : currentState(SystemState::MAIN_MENU), currentBoard() {}
 
 void GameManager::init() {
     // Initilize attack tables
+    static bool attacksInitialized = false;
+    if (!attacksInitialized) {
+        Chess::Attacks::init();
+        attacksInitialized = true;
+    }
     // Set chessboard to starting position
+    currentBoard.reset();
     // Set initial user settings
-
+    players[0] = PlayerSettings{false, false};
+    players[1] = PlayerSettings{false, false};
 }
+
+PlayerSettings& GameManager::getSettings(uint8_t player) { return players[player]; }
+
+void GameManager::setSettings(uint8_t playerNum, bool showBestMove, bool showLegalMoves) {
+    // out of bounds checking
+    if (playerNum >= 2 || playerNum < 0) return;
+
+    players[playerNum].showBestMove = showBestMove;
+    players[playerNum].showLegalMoves = showLegalMoves;
+}
+
+Chess::Board& GameManager::getBoard() { return currentBoard; }
--- src/Chess/game_manager.h
+++ src/Chess/game_manager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
+
 #include "chess.hpp"
 
 enum class SystemState { MAIN_MENU, PLAYING, ERROR_RECOVERY };
@@ -11,14 +12,26 @@ struct PlayerSettings {
 };
 
 class GameManager {
-public:
+   public:
+    // constructor
     GameManager();
+
+    // initialize game to starting state
     void init();
+
+    // new board state
     void updateBoard(uint64_t newBoard);
+
+    // change turns
     PlayerSettings& getSettings(uint8_t player);  // player: 0 or 1
+
+    // set settings
+    void setSettings(uint8_t playerNum, bool showBestMove, bool showLegalMoves);
+
+    // returns the board
     Chess::Board& getBoard();
 
-private:
+   private:
     SystemState currentState;
     Chess::Board currentBoard;
     PlayerSettings players[2];
